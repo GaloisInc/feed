aa225f7b56e51e7f53267d01155c0760728229e4
Sigbjorn Finne <sof@galois.com>
2007-12-06 18:03:09 -0800

    Simplified; parse-n-serialize any feed format

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,32 +1,13 @@
 module Main where
 
 import XML
-import RSS.Import
-import RSS.Export
-import RSS.Syntax
-
-import RSS1.Import as RSS1
-import RSS1.Export as RSS1
+import Feed.Import
+import Feed.Export
 
 import System.Environment
-import Data.Maybe
-import Control.Monad
-
-readRSS2 :: XML.Element -> Maybe String
-readRSS2 e = fmap (ppTopElement . xmlRSS) $ elementToRSS e
-
-readRSS1 :: XML.Element -> Maybe String
-readRSS1 e = fmap (ppTopElement . RSS1.xmlFeed) $ RSS1.elementToFeed e
 
 main :: IO ()
 main = do
   (x:_) <- getArgs
-  ls <- readFile x
-  case parseXMLDoc ls of
-    Nothing -> putStrLn "no parse"
-    Just d  -> 
-      case readRSS1 d `mplus` readRSS2 d of
-        Nothing -> putStrLn "not well-formed RSS input"
-	Just s  -> do
-	  putStrLn "Parsed RSS input OK"
-	  putStrLn s
+  feed  <- parseFeedFromFile x
+  putStrLn (ppTopElement $ xmlFeed feed)
