c6ec219f0360d707e8ccf387b6c75f85a9450d54
Sigbjorn Finne <sigbjorn.finne@gmail.com>
2007-12-10 09:28:23 -0800

    Allow feed content that isn't well-formed wrt feed specs, but well-formed XML

--- Feed/Types.hs
+++ Feed/Types.hs
@@ -3,15 +3,20 @@ module Feed.Types where
 import RSS.Syntax  as RSS
 import Atom.Feed   as Atom
 import RSS1.Syntax as RSS1
+import XML as XML
 
 -- Initially, we just wrap up the different feed formats in a sum type.
 data Feed
  = AtomFeed Atom.Feed
  | RSSFeed  RSS.RSS
  | RSS1Feed RSS1.Feed
+    -- if we're unable to correctly the well-formed XML as a feed,
+    -- keep it as an untyped document.
+ | XMLFeed  XML.Element
  
 data Item
  = AtomItem Atom.Entry
  | RSSItem  RSS.RSSItem
  | RSS1Item RSS1.Item
+ | XMLItem  XML.Element
  
