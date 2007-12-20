65e9fcad2f9d458a0c6ae2c3fb617f1b4427c7a3
Sigbjorn Finne <sof@galois.com>
2007-12-20 08:52:24 -0800

    Constructing feed items in a portable fashion, ver. 0.1

--- Feed/Constructor.hs
+++ Feed/Constructor.hs
@@ -1,12 +1,24 @@
-module Feed.Constructor where
+module Feed.Constructor 
+       ( FeedKind(..)
+       , newFeed       -- :: FeedKind -> Feed
+       , getFeedKind   -- :: Feed     -> FeedKind
+       , addItem       -- :: Item -> Feed -> Feed
+
+       , newItem       -- :: FeedKind   -> Item
+       , withItemTitle -- :: String     -> Item -> Item
+       , withItemLink  -- :: URLString  -> Item -> Item
+       , withItemDate  -- :: DateString -> Item -> Item
+       ) where
 
 import Feed.Types
 
 import Atom.Feed as Atom
 import RSS.Syntax as RSS
 import RSS1.Syntax as RSS1
+import DublinCore.Types
+import XML
 
-import Data.Maybe ( fromMaybe )
+import Data.Maybe ( fromMaybe, mapMaybe )
 
 data FeedKind
  = AtomKind
@@ -34,6 +46,7 @@ getFeedKind f =
     Feed.Types.AtomFeed{} -> AtomKind
     Feed.Types.RSSFeed r  -> RSSKind (case RSS.rssVersion r of { "2.0" -> Nothing; v -> Just v})
     Feed.Types.RSS1Feed r -> RDFKind (case RSS1.feedVersion r of { "1.0" -> Nothing; v -> Just v})
+    Feed.Types.XMLFeed{}  -> RSSKind (Just "2.0") -- for now, just a hunch..
 
 addItem :: Feed.Types.Item -> Feed.Types.Feed -> Feed.Types.Feed
 addItem it f = 
@@ -61,3 +74,87 @@ newItem fk =
                Feed.Types.RSS1Item (RSS1.nullItem "dummy-item-uri"
 						  "dummy-item-title"
 						  "dummy-item-link")
+
+-- | 'withItemDate updDate' associates the last-updated date, 'updDate',
+-- with a feed item.
+withItemDate :: DateString -> Feed.Types.Item -> Feed.Types.Item
+withItemDate dt fi = 
+  case fi of
+    Feed.Types.AtomItem e ->
+      Feed.Types.AtomItem e{Atom.entryUpdated=dt}
+    Feed.Types.RSSItem i  ->
+      Feed.Types.RSSItem  i{RSS.rssItemPubDate=Just dt}
+    Feed.Types.RSS1Item i ->
+      case break isDate $ RSS1.itemDC i of
+       (as,(dci:bs)) -> Feed.Types.RSS1Item i{RSS1.itemDC=as++dci{dcText=dt}:bs}
+       (_,[]) -> Feed.Types.RSS1Item i{RSS1.itemDC=DCItem{dcElt=DC_Date,dcText=dt}:RSS1.itemDC i}
+    Feed.Types.XMLItem i  ->
+      Feed.Types.XMLItem $
+        addChild (leaf (unqual "pubDate") dt) $
+	    filterChildren (\ e -> elName e /= unqual "pubDate")
+	                   i
+ where
+  isDate dc  = dcElt dc == DC_Date
+
+-- | 'withItemTitle myTitle' associates a new title, 'myTitle',
+-- with a feed item.
+withItemTitle :: String -> Feed.Types.Item -> Feed.Types.Item
+withItemTitle tit fi = 
+  case fi of
+    Feed.Types.AtomItem e ->
+      Feed.Types.AtomItem e{Atom.entryTitle=TextString tit}
+    Feed.Types.RSSItem i  ->
+      Feed.Types.RSSItem  i{RSS.rssItemTitle=Just tit}
+    Feed.Types.RSS1Item i ->
+      Feed.Types.RSS1Item  i{RSS1.itemTitle=tit}
+    Feed.Types.XMLItem i  ->
+      Feed.Types.XMLItem $
+        addChild (leaf (unqual "title") tit) $
+	    filterChildren (\ e -> elName e /= unqual "title")
+	                   i
+
+-- | 'withItemTitle myLink' associates a new URL, 'myLink',
+-- with a feed item.
+withItemLink :: URLString -> Feed.Types.Item -> Feed.Types.Item
+withItemLink url fi = 
+  case fi of
+    Feed.Types.AtomItem e ->
+      Feed.Types.AtomItem e{Atom.entryLinks=replaceAlternate url (Atom.entryLinks e)}
+    Feed.Types.RSSItem i  ->
+      Feed.Types.RSSItem  i{RSS.rssItemLink=Just url}
+    Feed.Types.RSS1Item i ->
+      Feed.Types.RSS1Item  i{RSS1.itemLink=url}
+    Feed.Types.XMLItem i  ->
+      Feed.Types.XMLItem $
+        addChild (leaf (unqual "link") url) $
+	    filterChildren (\ e -> elName e /= unqual "link")
+	                   i
+ where
+  replaceAlternate _ [] = []
+  replaceAlternate x (lr:xs) 
+   | toStr (Atom.linkRel lr) == "alternate" = lr{Atom.linkHref=x} : xs
+   | otherwise = lr : replaceAlternate x xs
+
+  toStr Nothing = ""
+  toStr (Just (Left x)) = x
+  toStr (Just (Right x)) = x
+    
+-- helpers..
+
+filterChildren :: (XML.Element -> Bool) -> XML.Element -> XML.Element
+filterChildren pre e = 
+  case elContent e of
+    Nothing -> e
+    Just cs -> e{elContent=Just (mapMaybe filterElt cs)}
+ where
+   filterElt xe@(XML.Elem el) 
+     | pre el    = Just xe
+     | otherwise = Nothing
+   filterElt xe  = Just xe
+
+addChild :: XML.Element -> XML.Element -> XML.Element
+addChild a b = 
+ case elContent b of
+   Nothing -> b{elContent=Just [XML.Elem a]}
+   Just xs -> b{elContent=Just (XML.Elem a : xs)}
+   
