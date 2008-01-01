209c2d36c156bd3001108f6eb50b93219366cce3
Sigbjorn Finne <sigbjorn.finne@gmail.com>
2008-01-01 08:08:57 -0800

    added a handful of new item constructors; covers RSS2.0 facilities pretty much now

--- Feed/Constructor.hs
+++ Feed/Constructor.hs
@@ -1,14 +1,20 @@
 module Feed.Constructor 
        ( FeedKind(..)
-       , newFeed       -- :: FeedKind -> Feed
-       , getFeedKind   -- :: Feed     -> FeedKind
-       , addItem       -- :: Item -> Feed -> Feed
+       , newFeed         -- :: FeedKind -> Feed
+       , getFeedKind     -- :: Feed     -> FeedKind
+       , addItem         -- :: Item -> Feed -> Feed
 
-       , newItem        -- :: FeedKind   -> Item
-       , withItemTitle  -- :: String     -> Item -> Item
-       , withItemLink   -- :: URLString  -> Item -> Item
-       , withItemDate   -- :: DateString -> Item -> Item
-       , withCategories -- :: [(String, Maybe String)] -> Item -> Item
+       , newItem          -- :: FeedKind   -> Item
+       , withItemTitle    -- :: String     -> Item -> Item
+       , withItemLink     -- :: URLString  -> Item -> Item
+       , withItemDate     -- :: DateString -> Item -> Item
+       , withItemAuthor   -- :: String     -> Item -> Item
+       , withItemCommentLink -- :: String     -> Item -> Item
+       , withItemEnclosure   -- :: String  -> Maybe String -> Integer -> Item -> Item
+       , withItemFeedLink    -- :: String -> String -> Item -> Item
+       , withItemId       -- :: Bool -> String -> Item -> Item
+       , withCategories   -- :: [(String, Maybe String)] -> Item -> Item
+       , withDescription  -- :: String     -> Item -> Item
        ) where
 
 import Feed.Types
@@ -20,6 +26,7 @@ import DublinCore.Types
 import Text.XML.Light as XML
 
 import Data.Maybe ( fromMaybe, mapMaybe )
+import Data.Char  ( toLower )
 
 -- | Construct an empty feed document, intending to output it in 
 -- the 'fk' feed format.
@@ -73,7 +80,8 @@ newItem fk =
 						  "dummy-item-link")
 
 -- | 'withItemDate updDate' associates the last-updated date, 'updDate',
--- with a feed item.
+-- with a feed item. If the RSS variant doesn't support the notion of
+-- last-updated, 'updDate' is set equal to the creation time.
 withItemDate :: DateString -> Feed.Types.Item -> Feed.Types.Item
 withItemDate dt fi = 
   case fi of
@@ -110,6 +118,132 @@ withItemTitle tit fi =
 	    filterChildren (\ e -> elName e /= unqual "title")
 	                   i
 
+-- | 'withItemAuthor auStr' associates new author info
+-- with a feed item.
+withItemAuthor :: String -> Feed.Types.Item -> Feed.Types.Item
+withItemAuthor au fi = 
+  case fi of
+    Feed.Types.AtomItem e ->
+      Feed.Types.AtomItem e{Atom.entryAuthors=[nullPerson{personName=au}]}
+    Feed.Types.RSSItem i  ->
+      Feed.Types.RSSItem  i{RSS.rssItemAuthor=Just au}
+    Feed.Types.RSS1Item i ->
+      case break isAuthor $ RSS1.itemDC i of
+       (as,(dci:bs)) -> Feed.Types.RSS1Item i{RSS1.itemDC=as++dci{dcText=au}:bs}
+       (_,[]) -> Feed.Types.RSS1Item i{RSS1.itemDC=DCItem{dcElt=DC_Creator,dcText=au}:RSS1.itemDC i}
+    Feed.Types.XMLItem i  ->
+      Feed.Types.XMLItem $
+        addChild (node (unqual "author",au)) $
+	    filterChildren (\ e -> elName e /= unqual "author")
+	                   i
+ where
+  isAuthor dc  = dcElt dc == DC_Creator
+
+-- | 'withItemFeedLink name myFeed' associates the parent feed URL 'myFeed'
+-- with a feed item. It is labelled as 'name'.
+withItemFeedLink :: String -> String -> Feed.Types.Item -> Feed.Types.Item
+withItemFeedLink tit url fi = 
+  case fi of
+    Feed.Types.AtomItem e ->
+      Feed.Types.AtomItem e{Atom.entrySource=Just Atom.nullSource{sourceId=Just url,sourceTitle=Just (TextString tit)}}
+    Feed.Types.RSSItem i  ->
+      Feed.Types.RSSItem  i{RSS.rssItemSource=Just (RSS.nullSource url tit)}
+    Feed.Types.RSS1Item i ->
+      Feed.Types.RSS1Item  i{RSS1.itemTitle=tit}
+    Feed.Types.XMLItem i  ->
+      Feed.Types.XMLItem $
+        addChild (node (unqual "source",Attr (unqual "url") url,tit)) $
+	    filterChildren (\ e -> elName e /= unqual "source")
+	                   i
+
+
+
+-- | 'withItemCommentLink url' sets the URL reference to the comment page to 'url'.
+withItemCommentLink :: String -> Feed.Types.Item -> Feed.Types.Item
+withItemCommentLink url fi = 
+  case fi of
+    Feed.Types.AtomItem e ->
+      Feed.Types.AtomItem e{Atom.entryLinks=((nullLink url){linkRel=Just (Left "replies")}):Atom.entryLinks e}
+    Feed.Types.RSSItem i  ->
+      Feed.Types.RSSItem  i{RSS.rssItemComments=Just url}
+    Feed.Types.RSS1Item i ->
+      case break isRel $ RSS1.itemDC i of
+       (as,(dci:bs)) -> Feed.Types.RSS1Item i{RSS1.itemDC=as++dci{dcText=url}:bs}
+       (_,[]) -> Feed.Types.RSS1Item i{RSS1.itemDC=DCItem{dcElt=DC_Relation,dcText=url}:RSS1.itemDC i}
+    Feed.Types.XMLItem i  ->
+      Feed.Types.XMLItem $
+        addChild (node (unqual "comments",url)) $
+	    filterChildren (\ e -> elName e /= unqual "comments")
+	                   i
+ where
+  isRel dc  = dcElt dc == DC_Relation
+
+-- | 'withItemEnclosure url mbTy len' sets the URL reference to the comment page to 'url'.
+withItemEnclosure :: String -> Maybe String -> Integer -> Feed.Types.Item -> Feed.Types.Item
+withItemEnclosure url ty len fi = 
+  case fi of
+    Feed.Types.AtomItem e -> Feed.Types.AtomItem 
+       e{Atom.entryLinks=((nullLink url){linkRel=Just (Left "enclosure")
+       				        ,linkType=ty
+					,linkLength=Just (show len)
+					}):Atom.entryLinks e}
+    Feed.Types.RSSItem i  ->
+      Feed.Types.RSSItem  i{RSS.rssItemEnclosure=Just (nullEnclosure url len (fromMaybe "text/html" ty))}
+    Feed.Types.RSS1Item i -> Feed.Types.RSS1Item 
+          i{RSS1.itemContent=nullContentInfo{ contentURI=Just url
+					    , contentFormat=ty
+					    }:RSS1.itemContent i}
+    Feed.Types.XMLItem i  ->
+      Feed.Types.XMLItem $
+        addChild ((node (unqual "enclosure",url))
+	  {elAttribs= [ Attr (unqual "length") "0"
+	  	      , Attr (unqual "type") (fromMaybe "text/html" ty)
+		      ]}) $
+	    filterChildren (\ e -> elName e /= unqual "enclosure")
+	                   i
+
+
+-- | 'withItemId isURL id' associates new unique identifier with a feed item.
+-- If 'isURL' is 'True', then the id is assumed to point to a valid web resource.
+withItemId :: Bool -> String -> Feed.Types.Item -> Feed.Types.Item
+withItemId isURL idS fi = 
+  case fi of
+    Feed.Types.AtomItem e ->
+      Feed.Types.AtomItem e{Atom.entryId=idS}
+    Feed.Types.RSSItem i  ->
+      Feed.Types.RSSItem  i{RSS.rssItemGuid=Just (nullGuid idS){rssGuidPermanentURL=Just isURL}}
+    Feed.Types.RSS1Item i ->
+      case break isId $ RSS1.itemDC i of
+       (as,(dci:bs)) -> Feed.Types.RSS1Item i{RSS1.itemDC=as++dci{dcText=idS}:bs}
+       (_,[]) -> Feed.Types.RSS1Item i{RSS1.itemDC=DCItem{dcElt=DC_Identifier,dcText=idS}:RSS1.itemDC i}
+    Feed.Types.XMLItem i  ->
+      Feed.Types.XMLItem $
+        addChild (node (unqual "guid",Attr (unqual "isPermaLink") (showBool isURL),idS)) $
+	    filterChildren (\ e -> elName e /= unqual "guid")
+	                   i
+ where
+  showBool x  = map toLower (show x)
+  isId dc     = dcElt dc == DC_Identifier
+
+-- | 'withDescription desc' associates a new descriptive string
+-- with a feed item.
+withDescription :: String -> Feed.Types.Item -> Feed.Types.Item
+withDescription desc fi = 
+  case fi of
+    Feed.Types.AtomItem e ->
+      Feed.Types.AtomItem e{Atom.entrySummary=Just (TextString desc)}
+    Feed.Types.RSSItem i  ->
+      Feed.Types.RSSItem  i{RSS.rssItemDescription=Just desc}
+    Feed.Types.RSS1Item i ->
+      Feed.Types.RSS1Item  i{RSS1.itemDesc=Just desc}
+    Feed.Types.XMLItem i  ->
+      Feed.Types.XMLItem $
+        addChild (node (unqual "description",desc)) $
+	    filterChildren (\ e -> elName e /= unqual "description")
+	                   i
+
+
+
 -- | 'withItemTitle myLink' associates a new URL, 'myLink',
 -- with a feed item.
 withItemLink :: URLString -> Feed.Types.Item -> Feed.Types.Item
@@ -136,11 +270,14 @@ withItemLink url fi =
   toStr (Just (Left x)) = x
   toStr (Just (Right x)) = x
     
-withCategories :: [(String,Maybe String)] -> Feed.Types.Item -> Feed.Types.Item
+withCategories :: [(String,Maybe String)]
+	       -> Feed.Types.Item
+	       -> Feed.Types.Item
 withCategories cats fi = 
   case fi of
     Feed.Types.AtomItem e ->
-      Feed.Types.AtomItem e{Atom.entryCategories=map ( \ (t,mb) -> (Atom.newCategory t){Atom.catScheme=mb})
+      Feed.Types.AtomItem 
+        e{Atom.entryCategories=map ( \ (t,mb) -> (Atom.newCategory t){Atom.catScheme=mb})
                                                      cats ++ entryCategories e}
     Feed.Types.RSSItem i  ->
       Feed.Types.RSSItem  i{RSS.rssItemCategories=
